18f6b4f4b0549e932ec6944d3eae52d8a6227074
Kenneth W.G. <kenwillg85@gmail.com>
2025-05-21 11:29:41 +0700

    feat: routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import HomePage from "./pages/Home";
+import MetodeTertutup from "./pages/metode_tertutup";
 import LoginPage from "./pages/login";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route index element={<HomePage />} />
+      <Route path="/metode_tertutup" element={<MetodeTertutup />} />
       <Route path="/login" element={<LoginPage />} />
     </Route>
   )
---
+++ src/pages/metode_tertutup/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MetodeTertutup = () => {
+  return (
+    <div>
+      <h1>Metode Tertutup</h1>
+    </div>
+  );
+};
+
+export default MetodeTertutup;
